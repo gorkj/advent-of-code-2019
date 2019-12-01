466b184afc78506c041e536c344c2dec09839bbc
Göran Kjellén <goran.kjellen@wirelesscar.com>
2019-12-01 15:59:54 +0100

    Day 1b completed.

--- src/advent_of_code_2019/core.clj
+++ src/advent_of_code_2019/core.clj
@@ -3,19 +3,35 @@
   (:gen-class))
 
 (defn calc-fuel
+  "Calculate the amount of fuel required for a mass."
   [mass]
-  (- (quot mass 3) 2)
+  (let [res (- (quot mass 3) 2)]
+    (if (neg? res) 0 res))
   )
+
+(defn calc-fuel-with-fuel
+  "Calculate total fuel including fuel to carry the extra load of the fuel itself."
+  [mass]
+  (->>(iterate calc-fuel mass)
+      (drop 1)
+      (take-while #(not= 0 %))
+      (reduce +)))
 
 (defn -main
   "Runs all the problems sequentially"
   [& args]
   (print "Day 1a: ")
-  (println
-   (->> (io/resource "day-1.csv")
-        io/reader
-        line-seq
-        (map #(Integer/parseInt %))
-        (map calc-fuel)
-        (reduce +)
-        )))
+  (println (->> (io/resource "day-1.csv")
+                io/reader
+                line-seq
+                (map #(Integer/parseInt %))
+                (map calc-fuel)
+                (reduce +)))
+  (print "Day 1b: ")
+  (println (->> (io/resource "day-1.csv")
+                io/reader
+                line-seq
+                (map #(Integer/parseInt %))
+                (map calc-fuel-with-fuel)
+                (reduce +)))
+  )
--- test/advent_of_code_2019/core_test.clj
+++ test/advent_of_code_2019/core_test.clj
@@ -2,9 +2,15 @@
   (:require [clojure.test :refer :all]
             [advent-of-code-2019.core :refer :all]))
 
-(deftest day-1-test
+(deftest day-1a-test
   (testing "Day 1 Fuel consumption"
     (is (= (calc-fuel 12) 2))
     (is (= (calc-fuel 14) 2))
     (is (= (calc-fuel 1969) 654))
     (is (= (calc-fuel 100756) 33583))))
+
+(deftest day-1b-test
+  (testing "Day 1 Fuel consumption including mass of fuel"
+    (is (= (calc-fuel-with-fuel 14) 2))
+    (is (= (calc-fuel-with-fuel 1969) 966))
+    (is (= (calc-fuel-with-fuel 100756) 50346))))
