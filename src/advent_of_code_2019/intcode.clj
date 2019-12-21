2dd94c34a1fb63cc211d1bce8fc7b4d8429de4db
Göran Kjellén <goran@kjellen.nu>
2019-12-22 00:17:29 +0100

    Refactored reading of parameters.

--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -11,45 +11,7 @@
 
 (declare intcomp)
 
-(defn- get-param [mode value memory]
-  (if (= :position mode) (nth memory value) value))
-
-(defn- get-address [mode value memory]
-  (if (= :immediate mode) (nth memory value) value))
-
-(defn- add [index memory]
-  (let [[op-and-modes a b loc] (drop index memory)
-        [op m1 m2 m3] (read-op op-and-modes)]
-    (intcomp (+ index 4)
-             (assoc memory
-                    (get-address m3 loc memory)
-                    (+ (get-param m1 a memory)
-                       (get-param m2 b memory))))))
-
-(defn- mul [index memory]
-  (let [[op-and-modes a b loc] (drop index memory)
-        [op m1 m2 m3] (read-op op-and-modes)]
-    (intcomp (+ index 4)
-             (assoc memory
-                    (get-address m3 loc memory)
-                    (* (get-param m1 a memory)
-                       (get-param m2 b memory))))))
-
-(defn- input [index memory]
-  (let [[op-and-modes loc] (drop index memory)
-        [op m1] (read-op op-and-modes)]
-    (intcomp (+ index 2)
-             (assoc memory
-                    (get-address m1 loc memory)
-                    (edn/read-string (read-line))))))
-
-(defn- output [index memory]
-  (let [[op-and-modes loc] (drop index memory)
-        [op m1] (read-op op-and-modes)]
-    (println (get memory (get-address m1 loc memory)))
-    (intcomp (+ index 2) memory)))
-
-(defn- read-op
+(defn- parse-modes
   [number]
   (let [[m3 m2 m1 & op] (format "%05d" number)]
     [(Integer/parseInt (apply str op))
@@ -57,19 +19,55 @@
      (if (= \0 m2) :position :immediate)
      (if (= \0 m3) :position :immediate)]))
 
+(defn- oplen [op]
+  (case op 1 4 2 4 3 2 4 2 5 2 6 2 7 3 8 3 99 0))
+
+(defn- read-param [value mode memory]
+  (if (= :position mode) (nth memory value) value))
+
+(defn- read-op
+  [index memory]
+  (let [prg (drop index memory)
+        [op & modes] (parse-modes (first prg))
+        [_ & params-raw] (take (oplen op) prg)
+        params (map #(read-param %1 %2 memory) (drop-last params-raw) modes)
+        address (last params-raw)]
+    [op params address]))
+
+(defn- add [params address index memory]
+  (intcomp (+ index 4)
+           (assoc memory
+                  address
+                  (+ (first params) (second params)))))
+
+(defn- mul [params address index memory]
+  (intcomp (+ index 4)
+           (assoc memory
+                  address
+                  (* (first params) (second params)))))
+
+(defn- input [address index memory]
+  (intcomp (+ index 2)
+           (assoc memory
+                  address
+                  (edn/read-string (read-line)))))
+
+(defn- output [address index memory]
+  (println (get memory address))
+  (intcomp (+ index 2) memory))
+
 (defn intcomp
   ([memory]
    (intcomp 0 memory))
   ([index memory]
-   (let [[op] (read-op (get memory index))]
+   (let [[op params address] (read-op index memory)]
      (case op
-       1 (add index memory)
-       2 (mul index memory)
-       3 (input index memory)
-       4 (output index memory)
+       1 (add params address index memory)
+       2 (mul params address index memory)
+       3 (input address index memory)
+       4 (output address index memory)
        99 memory))))
 
 (comment
   (intcomp [1002 4 3 4 33])
   (intcomp (get-int-program)))
-
