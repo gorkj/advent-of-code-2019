62f4dee61b3393766f3208902e5ffb000079bd48
Göran Kjellén <goran@kjellen.nu>
2019-12-27 12:58:21 +0100

    Added multiple input support.

--- src/advent_of_code_2019/intcomp-mk2.clj
+++ src/advent_of_code_2019/intcomp-mk2.clj
@@ -51,11 +51,10 @@
                   (* (first params) (second params)))))
 
 (defn- input [in out address index memory]
-  (intcomp-int in out (+ index 2)
-           (assoc memory
-                  address
-                  in
-                  #_(edn/read-string (read-line)))))
+  (intcomp-int (rest in) out (+ index 2)
+               (assoc memory
+                      address
+                      (first in))))
 
 (defn- output [in out params index memory]
   ;;(info "output" (first params) index)
@@ -113,39 +112,43 @@
   (testing "compare to eight"
     (testing "position mode"
       (let [prg [3,9,8,9,10,9,4,9,99,-1,8]]
-        (is (= 0 (intcomp 7 prg)))
-        (is (= 1 (intcomp 8 prg)))
-        (is (= 0 (intcomp 9 prg))))
+        (is (= 0 (intcomp [7] prg)))
+        (is (= 1 (intcomp [8] prg)))
+        (is (= 0 (intcomp [9] prg))))
       (let [prg [3,9,7,9,10,9,4,9,99,-1,8]]
-        (is (= 1 (intcomp 7 prg)))
-        (is (= 0 (intcomp 8 prg)))))
+        (is (= 1 (intcomp [7] prg)))
+        (is (= 0 (intcomp [8] prg)))))
     (testing "immediate mode"
       (let [prg [3,3,1108,-1,8,3,4,3,99]]
-        (is (= 0 (intcomp 7 prg)))
-        (is (= 1 (intcomp 8 prg)))
-        (is (= 0 (intcomp 9 prg))))
+        (is (= 0 (intcomp [7] prg)))
+        (is (= 1 (intcomp [8] prg)))
+        (is (= 0 (intcomp [9] prg))))
       (let [prg [3,3,1107,-1,8,3,4,3,99]]
-        (is (= 1 (intcomp 7 prg)))
-        (is (= 0 (intcomp 8 prg))))))
+        (is (= 1 (intcomp [7] prg)))
+        (is (= 0 (intcomp [8] prg))))))
   (testing "zero or not"
     (let [prg [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]]
-      (is (= 0 (intcomp 0 prg)))
-      (is (= 1 (intcomp 1 prg)))
-      (is (= 1 (intcomp 42 prg))))
+      (is (= 0 (intcomp [0] prg)))
+      (is (= 1 (intcomp [1] prg)))
+      (is (= 1 (intcomp [42] prg))))
     (let [prg [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]]
-      (is (= 0 (intcomp 0 prg)))
-      (is (= 1 (intcomp 1 prg)))
-      (is (= 1 (intcomp 42 prg))))
+      (is (= 0 (intcomp [0] prg)))
+      (is (= 1 (intcomp [1] prg)))
+      (is (= 1 (intcomp [42] prg))))
     ))
 
 (deftest jumping
   (testing "Jump program"
     (let [prg [3 21,1008 21 8 20,1005 20 22,107 8 21 20,1006 20 31,1106 0 36,98 0 0,1002 21 125 20,
                4 20,1105 1 46,104 999,1105 1 46,1101 1000 1 20,4 20,1105 1 46 98,99]]
-      (is (= 999 (intcomp 7 prg )))
-      (is (= 1000 (intcomp 8 prg )))
-      (is (= 1001 (intcomp 9 prg ))))))
+      (is (= 999 (intcomp [7] prg )))
+      (is (= 1000 (intcomp [8] prg )))
+      (is (= 1001 (intcomp [9] prg ))))))
 
-;;[3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0] [4 3 2 1 0] -> 43210
+(let [prg [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
+      phases [4 3 2 1 0]]
+  (map #(intcomp % prg))
+  )
+;; 43210
 
 
